382945e2dcc6dc09eeb92b8e0ef6d1382b5d53a6
joshmarten <josh.marten@icloud.com>
2016-11-06 23:03:59 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ var url;
 
 var quote;
 
+var price;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	url = base + symbol;
@@ -18,6 +20,47 @@ function getData(data) {
 
 function draw() {
 	background(255);
-	text(quote.ChangePercent,20,20);
+	if (quote) {
+		noStroke();
+		textAlign(CENTER);
+		textSize(height / 15);
+		fill(100);
+		text(quote.Name, width / 2, height / 8);
+		textSize(height / 20);
+		text(quote.Symbol, width / 2, height / 5);
 
+		price = quote.LastPrice;
+
+		map(price, quote.Low, quote.High, 0, width);
+
+		strokeWeight(height / 10);
+		stroke('red');
+		line(0, height / 2, price, height / 2);
+
+		strokeWeight(height / 10);
+		stroke('green');
+		line(width, height / 2, price, height / 2);
+
+		noStroke();
+		fill(255);
+		ellipse(price, height / 2, height / 10, height / 10);
+
+		noStroke();
+		textAlign(CENTER);
+		textSize(height / 35);
+		fill(100);
+		text("Current Price: " + "$" + quote.LastPrice, width / 2, height * 3 / 5);
+
+		noStroke();
+		textAlign(LEFT);
+		textSize(height / 40);
+		fill(100);
+		text("Low: " + "$" + quote.Low, width/40, height * 3 / 5);
+		
+		noStroke();
+		textAlign(RIGHT);
+		textSize(height / 40);
+		fill(100);
+		text("High: " + "$" + quote.High, width*39/40, height * 3 / 5);
+	}
 }
