0e2079560ae0d19e5b3e984988aaa6e82c3e8dc3
joshmarten <josh.marten@icloud.com>
2016-11-06 19:11:55 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
-function setup() {
+var base = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=';
+var symbol = 'AAPL';
 
+var url;
+
+var quote;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	url = base + symbol;
+	loadJSON(url, getData);
+}
+
+function getData(data) {
+	quote = data;
+	console.log(quote);
 }
 
 function draw() {
-  
+
 }
